d0e09bdfaa9550e5bf0a2dcf5b00aff2aac21d50
liuyuelintop <liuyuelintop@gmail.com>
2024-06-21 15:03:28 +1000

    Populating the Test Db and Testing Routes with Parameters

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,4 +1,5 @@
 const request = require("supertest");
+// const mongoose = require("mongoose");
 const { Genre } = require("../../models/genre");
 let server;
 
@@ -6,12 +7,6 @@ describe("/api/genres", () => {
   beforeEach(async () => {
     process.env.PORT = 3001; // 设置测试端口为 3001
     server = require("../../index");
-    await Genre.deleteMany({}); // 清空数据库
-    await Genre.insertMany([
-      { name: "Action" },
-      { name: "Horror" },
-      { name: "Comedy" },
-    ]);
   });
 
   afterEach(async () => {
@@ -20,6 +15,12 @@ describe("/api/genres", () => {
 
   describe("GET /", () => {
     it("should return all genres", async () => {
+      await Genre.deleteMany({}); // 清空数据库
+      await Genre.insertMany([
+        { name: "Action" },
+        { name: "Horror" },
+        { name: "Comedy" },
+      ]);
       const res = await request(server).get("/api/genres");
       expect(res.status).toBe(200);
       expect(res.body.length).toBe(3);
