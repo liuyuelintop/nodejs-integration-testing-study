f485eea064eefb7885fbd7945997bd472214ca75
liuyuelintop <liuyuelintop@gmail.com>
2024-06-21 19:29:05 +1000

    Testing the Auth Middleware

---
+++ tests/integration/auth.test.js
@@ -0,0 +1,49 @@
+const { User } = require("../../models/user");
+const { Genre } = require("../../models/genre");
+const request = require("supertest");
+
+describe("auth middleware", () => {
+  beforeEach(() => {
+    process.env.PORT = 3001; // 设置测试端口为 3001
+    server = require("../../index");
+  });
+  afterEach(async () => {
+    await Genre.deleteMany({});
+    server.close();
+  });
+
+  let token;
+
+  const exec = () => {
+    return request(server)
+      .post("/api/genres")
+      .set("x-auth-token", token)
+      .send({ name: "genre1" });
+  };
+
+  beforeEach(() => {
+    token = new User().generateAuthToken();
+  });
+
+  it("should return 401 if no token is provided", async () => {
+    token = "";
+
+    const res = await exec();
+
+    expect(res.status).toBe(401);
+  });
+
+  it("should return 400 if token is invalid", async () => {
+    token = "a";
+
+    const res = await exec();
+
+    expect(res.status).toBe(400);
+  });
+
+  it("should return 200 if token is valid", async () => {
+    const res = await exec();
+
+    expect(res.status).toBe(200);
+  });
+});
