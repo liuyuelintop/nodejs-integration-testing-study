beadc70fceb17c09fa5aa8ccda3ae9e549804989
liuyuelintop <liuyuelintop@gmail.com>
2024-06-23 21:41:51 +1000

    Mongoose Static Methods

--- routes/returns.js
+++ routes/returns.js
@@ -16,10 +16,7 @@ function validateReturn(req) {
   return schema.validate(req);
 }
 router.post("/", [auth, validate(validateReturn)], async (req, res) => {
-  const rental = await Rental.findOne({
-    "customer._id": req.body.customerId,
-    "movie._id": req.body.movieId,
-  });
+  const rental = await Rental.lookup(req.body.customerId, req.body.movieId);
 
   if (!rental) {
     return res.status(404).send("Rental not found!");
