c534a618e08e58cced250a16f978d0b09ae9bd6b
liuyuelintop <liuyuelintop@gmail.com>
2024-06-24 17:57:52 +1000

    Preparing the App for Heroku

--- startup/prod.js
+++ startup/prod.js
@@ -1,5 +1,5 @@
 const helmet = require("helmet");
-const compressions = require("compression");
+const compression = require("compression");
 
 module.exports = function (app) {
   app.use(helmet());
