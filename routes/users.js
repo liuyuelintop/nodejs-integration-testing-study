59bc538dcbd74b8eab73131b2e6736a873ee95bf
liuyuelintop <liuyuelintop@gmail.com>
2024-06-25 15:15:03 +1000

    complete routes and replace fawn using mongoose session

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,11 @@ const { User, validate } = require("../models/user");
 const auth = require("../middleware/auth");
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+  const users = await User.find().select("-password").sort("name");
+  res.send(users);
+});
+
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
@@ -10,14 +15,15 @@ router.post("/", async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("User already registered.");
 
-  user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  });
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
   await user.save();
 
-  res.send(user);
+  const token = user.generateAuthToken();
+  res
+    .header("x-auth-token", token)
+    .send(_.pick(user, ["_id", "name", "email"]));
 });
 
 module.exports = router;
--- startup/db.js
+++ startup/db.js
@@ -4,5 +4,8 @@ const logger = require("./logging"); // Import the logger
 
 module.exports = function () {
   const db = config.get("db");
-  mongoose.connect(db).then(() => logger.info(`Connected to ${db}...`)); // Use logger.info
+  mongoose
+    .connect(db)
+    .then(() => logger.info(`Connected to ${db}...`))
+    .catch((err) => logger.error("Could not connect to MongoDB...", err)); // Use logger.info
 };
